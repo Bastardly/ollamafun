75d8822dd1643328c623b889d3edb9b4476c07c6
Flemming Hansen <fha@kmd.dk>
2025-07-24 10:46:14 +0200

    Made the UI a bit better

--- static/app.js
+++ static/app.js
@@ -1,19 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let count = 0;
   const form = document.getElementById("promptForm");
   const promptInput = document.getElementById("prompt");
-  const responseBox = document.getElementById("responseBox");
   const errorBox = document.getElementById("errorBox");
-  const queryText = document.getElementById("query");
+  const conversation = document.querySelector("div.conversation-container");
+  const conversationWindow = document.querySelector("div.conversation-window");
+  const scrollFromTopOffset = 12; //12px
+
+  function pasteTextInPrompt(text) {
+    promptInput.value = text;
+    promptInput.select();
+  }
+
+  function addToConversation(type, text) {
+    const div = document.createElement("div");
+    const isPrompt = type === "prompt";
+    const classType = isPrompt ? "prompt" : "reply";
+    div.classList.add("conversation", classType);
+    div.textContent = text;
+    conversation.append(div);
+
+    if (isPrompt) {
+      count += 1;
+      div.id = "prompt-" + count;
+      const newDiv = document.getElementById(div.id);
+      if (newDiv && conversationWindow) {
+        conversationWindow.scrollTop =
+          newDiv.offsetTop - conversationWindow.offsetTop - scrollFromTopOffset;
+      }
+      div.onclick = () => pasteTextInPrompt(div.textContent);
+    }
+  }
+
+  const deleteMe = document.createElement("button");
+  deleteMe.innerText = "Create mock prompt";
+  deleteMe.onclick = () =>
+    addToConversation("prompt", "Hello from the mock button" + count);
+
+  form.appendChild(deleteMe);
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     if (promptInput.value.trim().length < 1) return;
-
     errorBox.textContent = "";
-    responseBox.textContent = "responding...";
 
-    responseBox.disabled = true;
     promptInput.disabled = true;
 
     const res = await fetch("/generate", {
@@ -29,15 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (!res.ok) {
       const text = await res.text();
-      errorBox.textContent = text;
+      errorBox.textContent = "ERROR: " + text;
       return;
     }
+    addToConversation("prompt", promptInput.value);
+    promptInput.value = "";
 
     const json = await res.json();
-    responseBox.disabled = false;
-    responseBox.textContent = json.response;
-    queryText.innerText = promptInput.value;
-    promptInput.value = "";
+    addToConversation("reply", json.response);
     promptInput.disabled = false;
     promptInput.focus();
   });
